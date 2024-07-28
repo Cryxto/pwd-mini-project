84e46e12fc5640cd92a8c03813c8de53083af5df
Cryxto <bagasadvan123@gmail.com>
2024-07-28 21:02:42 +0700

    feat: ux improvement

--- apps/web/src/components/Hero.tsx
+++ apps/web/src/components/Hero.tsx
@@ -5,11 +5,11 @@ function Hero() {
 
     <section
       id="hero"
-      className="w-full h-[93vh] relative self-center shadow-lg drop-shadow-2xl bg-black rounded-br-[20rem] mb-28"
+      className="w-full h-[93vh] relative self-center shadow-lg drop-shadow-2xl bg-black"
     >
       <div style={{position: 'relative', minHeight: '100%', minWidth: '100vw'}}>
         <Image
-          className="top-0 left-0 w-full h-[93vh] object-cover rounded-br-[20rem] shadow-2xl relative"
+          className="top-0 left-0 w-full h-[93vh] object-cover  shadow-2xl relative"
           src="https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="/"
           fill={true}
@@ -17,11 +17,11 @@ function Hero() {
           priority
         />
       </div>
-      <div className="bg-black/80 absolute top-0 left-0 w-full h-[93vh] rounded-br-[20rem]" />
+      <div className="bg-black/80 absolute top-0 left-0 w-full h-[93vh] " />
       <div className="absolute top-0 w-full h-full flex flex-col justify-center text-slate-100 items-center ">
         <div className="max-w-[1100px] absolute p-4 m-4 ">
           <p className="font-medium">Easy to</p>
-          <h1 className="font-bold text-5xl md:text-7xl drop-shadow-2xl rounded-br-[20rem]">
+          <h1 className="font-bold text-5xl md:text-7xl drop-shadow-2xl ">
             Make Your Idea Comes True
           </h1>
           <p className="max-w-[600px] drop-shadow-2xl py-2 text-xl leading-loose">
